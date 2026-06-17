e39cf67f78923330d5054eec0db877f169311660
deepu <deepukumarim25ai@vetias.ac.in>
2026-06-17 11:13:29 +0530

    day11 file added

---
+++ src/day11.java
@@ -0,0 +1,18 @@
+public class day11 {
+    static void main(){
+        String name = null;
+        try{
+            System.out.println(name.charAt(0));
+            System.out.println(45/0);
+        }catch (ArithmeticException e){
+            System.out.println("Arithmetic Exception");
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array Index Out Of Bounds Exception");
+        }catch (Exception e){
+            System.out.println("Exception");
+        }finally {
+            System.out.println("This will always execute.");
+        }
+        System.out.println("Still running.");
+    }
+}
