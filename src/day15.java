e0f9161f5e8cb330ff5dd43dec1cbcee1c184323
deepu <deepukumarim25ai@vetias.ac.in>
2026-06-23 10:29:04 +0530

    day15 file added

---
+++ src/day15.java
@@ -0,0 +1,18 @@
+public class day15 {
+     static void main() throws InterruptedException{
+         Runnable ChefTask = () -> {
+             String[] items = {"Ice cream","Dosa","Coffee"};
+             for (String item : items) {
+                 System.out.println("Cooking Items:" + item);
+                 try {
+                     Thread.sleep(2000);
+                 } catch (InterruptedException e) {
+                     throw new RuntimeException(e);
+                 }
+             }
+         };
+         Thread ChefThread = new Thread(ChefTask,"chef-thread");
+         ChefThread.start();
+         ChefThread.join();
+     }
+}
